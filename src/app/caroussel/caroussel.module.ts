d5857d5eefe99eb933394b5c4caa691f43e3004c
rnormando <42142498+rnormando@users.noreply.github.com>
2018-10-06 16:09:29 -0300

    Caroussel ok
    * Added right images to caroussel

--- src/app/caroussel/caroussel.module.ts
+++ src/app/caroussel/caroussel.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 
 @NgModule({
   imports: [
     CommonModule,
-    Ng2CarouselamosModule
   ],
   declarations: []
 })
