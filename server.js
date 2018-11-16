911e8ebf4c32ec843d22fc5c0947500111910a06
rnormando <42142498+rnormando@users.noreply.github.com>
2018-11-16 20:51:23 -0200

    Initial Heroku Deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/PIFRONTEND'));
+app.use(express.static(__dirname + '/dist/PI-Frontend'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/PIFRONTEND/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/PI-Frontend/index.html'));
 });
 
 // default Heroku port
