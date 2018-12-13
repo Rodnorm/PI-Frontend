1aa64a0675df92f8bdd23770695595d120d78114
Rodnorm <r.normando@hotmail.com>
2018-12-13 13:02:02 -0200

    userAccount finished
    :alien:

--- src/app/userAccount/userAccount.component.ts
+++ src/app/userAccount/userAccount.component.ts
@@ -12,8 +12,9 @@ export class PedidosComponent implements OnInit {
   private keyToken = 'Token';
   private keyLogin = 'Login';
   private orders;
+  private loader:boolean = false;
   updateInfo: FormGroup;
-  
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -21,37 +22,45 @@ export class PedidosComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    let token = localStorage.getItem(this.keyToken);
-    let login = localStorage.getItem(this.keyLogin);
-    this.GS.getUserDetails(login, token)
-    .subscribe( data => {
-      let userData = data['data'];
-      
-      this.updateInfo = this.formBuilder.group({
-        'nome': [userData.nome, Validators.compose([Validators.required, Validators.minLength(2)])],
-        'email': [userData.email, Validators.compose([Validators.required, Validators.email])],
-        'senha': [userData.senha, Validators.compose([Validators.minLength(8), Validators.required, Validators.maxLength(28)])],
-        'cpf': [userData.cpf, Validators.compose([Validators.required, Validators.minLength(11)])],
-        'logradouro': [userData.logradouro],
-        'numero': [userData.numero],
-        'complemento': [userData.complemento],
-        'cep': [userData.cep],
-        'bairro': [userData.bairro],
-        'cidade': [userData.cidade],
-        'uf': [userData.uf, Validators.minLength(2)]
-      });
-    });
-    this.getOrders(login, token);
-    
+
+    this.getUser();
+    this.getOrders();
   }
 
+  public getUser() {
 
-  private getOrders(login, token){
+
+    this.loader = true;
+    let token = localStorage.getItem(this.keyToken);
+    let login = localStorage.getItem(this.keyLogin);
+
+    this.GS.getUserDetails(login, token)
+      .subscribe(data => {
+        let userData = data['data'];
+        this.updateInfo = this.formBuilder.group({
+          'nome': [userData.nome, Validators.compose([Validators.required, Validators.minLength(2)])],
+          'email': [userData.email, Validators.compose([Validators.required, Validators.email])],
+          'senha': [userData.senha, Validators.compose([Validators.minLength(8), Validators.required, Validators.maxLength(28)])],
+          'cpf': [userData.cpf, Validators.compose([Validators.required, Validators.minLength(11)])],
+          'logradouro': [userData.logradouro],
+          'numero': [userData.numero],
+          'complemento': [userData.complemento],
+          'cep': [userData.cep],
+          'bairro': [userData.bairro],
+          'cidade': [userData.cidade],
+          'uf': [userData.uf, Validators.minLength(2)]
+        });
+        this.loader = false;
+      });
+  }
+  private getOrders() {
+
+    let token = localStorage.getItem(this.keyToken);
+    let login = localStorage.getItem(this.keyLogin);
     this.GS.getOrdersByUser(login, token)
-    .subscribe(response => {
-      this.orders = response['data'];
-      console.log(this.orders);
-      
-    });
+      .subscribe(response => {
+        this.orders = response['data'];
+        console.log(this.orders);
+      });
   }
 }
